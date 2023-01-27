ff507349d057b586ea05089f20ee1ad711847163
Sardar Badar Saghir <dataentrybadar@gmail.com>
2023-01-27 17:18:11 +0500

    added reddit Navbar search

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,17 +1,20 @@
-import { PhoneIcon, SearchIcon } from "@chakra-ui/icons";
+import { SearchIcon } from "@chakra-ui/icons";
 import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import React, { PropsWithChildren } from "react";
 
 type SearchInputProps = {} & PropsWithChildren;
 
 const SearchInput: React.FC<SearchInputProps> = () => {
-  return (<Flex>
+  return (<Flex align={"center"}>
   <InputGroup>
     <InputLeftElement
       pointerEvents='none'
       children={<SearchIcon color='gray.300' />}
     />
-    <Input type='text' placeholder='Search Reddit' />
+    <Input type='text' placeholder='Search Reddit' fontSize={'10pt'} _placeholder={{color:"gray.500"  }} _hover={{bg:'white',border:"1px solid", borderColor:"blue.500"}} _focus={{outline:"none",border:'1px solid',borderColor:"blue.500"}} 
+    height={"34px"}
+    bg={"gray.50"}
+    />
   </InputGroup>
 
   </Flex>);
