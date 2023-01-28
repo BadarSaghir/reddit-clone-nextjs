24a98742a17153e83a4caa4774a23052a0d81625
Sardar Badar Saghir <dataentrybadar@gmail.com>
2023-01-28 17:57:57 +0500

    added oatuh button

--- src/features/modal/Auth/OAuthButton.tsx
+++ src/features/modal/Auth/OAuthButton.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex,Image } from '@chakra-ui/react';
+import { Button, Flex,Image,Text } from '@chakra-ui/react';
 import React from 'react';
 
 type OAuthButtonProps = {
@@ -7,9 +7,10 @@ type OAuthButtonProps = {
 
 const OAuthButton:React.FC<OAuthButtonProps> = () => {
  
-     return <Flex direction={"column"} width="100%" mb={4}>
-        <Button variant={"oauth"}><Image src='/images/googlelogo.png' height={"20px"} mr={4}/> Continue With Google</Button>
-        <Button mt={2} variant={"oauth"}>Some Other Provider</Button>
+     return <Flex direction={"column"} width="100%" mb={4} _active={{color:"gray.900"}} color={"gray.500"} _hover={{color:"gray.600"}} >
+        <Button  variant={"oauth"}><Image src='/images/googlelogo.png' height={"20px"} mr={4}/> <Text    display={{base:"none",sm:"inherit"}}>
+        Continue With Google </Text> </Button>
+        {/* <Button mt={2} variant={"oauth"}>Some Other Provider</Button> */}
      </Flex>
 }
 export default OAuthButton;
