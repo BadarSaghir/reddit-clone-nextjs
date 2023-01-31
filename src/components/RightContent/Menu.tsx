211b4bafb6c306459ffa923057257a0a38c45abb
Sardar Badar Saghir <dataentrybadar@gmail.com>
2023-01-31 18:00:40 +0500

    added navbar

--- src/components/RightContent/Menu.tsx
+++ src/components/RightContent/Menu.tsx
@@ -11,12 +11,17 @@ import {  useAppDispatch } from "../../hooks/hooks";
 import { auth } from "../../firebase/clientApp";
 import { setUserInfo } from "../../features/modal/Auth/userInfoSlice";
 import {  navigationInitialState, navigationReducer } from "../../hooks/naviagationHook";
+import { openModalState } from "../../features/modal/Auth/authModalSlice";
 type MenuProps = {
 user:User|null
 };
-const menuList =[{id:0,icon:CgProfile, text:"Profile"},{id:1,icon:MdOutlineLogin, text:"Log Out"}]
+const menuListUser =[{id:0,icon:CgProfile, text:"Profile"},{id:1,icon:MdOutlineLogin, text:"Log Out"}]
+const menuListAuth =[{id:0,icon:MdOutlineLogin, text:"Log In / Sign Up"}]
+
 const Menu: React.FC<MenuProps> = ({user}) => {
+
 const [value, dispatch] = useReducer(navigationReducer,navigationInitialState)
+const appDispatch=useAppDispatch()
   const handleItemClick =(idx:number)=>{
 return async(e:React.MouseEvent<HTMLDivElement, MouseEvent>)=>{
   e.preventDefault()
@@ -31,27 +36,38 @@ break
   }
 }
   }
+
+
   return (
     <NavMenu>
       <MenuButton cursor={"pointer"}
       padding="0px 6px"
-      _hover={{border:"",outlineColor:"gray.200"}}
+      _hover={{outlineColor:"gray.200"}}
       >
         {user?<Flex align={"center"}><Icon as={FaRedditSquare} fontSize={24} color={"gray.300"} ml={1.5} mr={1.5}/>
         <ChevronDownIcon/>
         </Flex>:<Flex align={"center"}><Icon fontSize={24}  as={VscAccount} color={"gray.400"}  mr={1} /> <ChevronDownIcon/></Flex>}
       </MenuButton>
       <MenuList mt={0.5}>
-        {menuList.map((item,idx)=>{
+        {user?menuListUser.map((item,idx)=>{
 
-      return<>  <MenuItem key={idx} fontWeight={"700"} fontSize={"10pt"} >
+      return<>  <MenuItem bg={"white"} _hover={{bg:"blue.500",color:"white"}} key={idx} fontWeight={"700"} fontSize={"10pt"} >
         <Flex align={"center"} onClick={handleItemClick(item.id)}>
             <Icon fontSize={20} mr={2} as={item.icon} />
             {item.text}
         </Flex>
      
-        </MenuItem>   {idx<menuList.length-1 &&<MenuDivider/>}</>
-        })}
+        </MenuItem>   {idx<menuListUser.length-1 &&<MenuDivider/>}</>
+        }):menuListAuth.map((item,idx)=>{
+
+          return<>  <MenuItem bg={"white"} _hover={{bg:"blue.500",color:"white"}} key={idx} fontWeight={"700"} fontSize={"10pt"} >
+            <Flex align={"center"} onClick={()=>{appDispatch(openModalState("login"))}}>
+                <Icon fontSize={20} mr={2} as={item.icon} />
+                {item.text}
+            </Flex>
+         
+            </MenuItem>   {idx<menuListAuth.length-1 &&<MenuDivider/>}</>
+            })}
       </MenuList>
     </NavMenu>
   );
