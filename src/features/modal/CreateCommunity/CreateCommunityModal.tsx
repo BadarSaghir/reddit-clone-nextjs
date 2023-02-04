a34de2002e0bdc51cf10ba29e54137f6342883dd
Sardar Badar Saghir <dataentrybadar@gmail.com>
2023-02-04 10:54:16 +0500

    added check bug fix

--- src/features/modal/CreateCommunity/CreateCommunityModal.tsx
+++ src/features/modal/CreateCommunity/CreateCommunityModal.tsx
@@ -11,7 +11,7 @@ const CreateCommunityModal:React.FC<CreateCommunityModalProps> = ({open, handleO
     const CHARACTER_LENGTHS=21
     const [characterRemains, setCharacterRemain]=useState(CHARACTER_LENGTHS)
    const handleOnChange =(e: React.ChangeEvent<HTMLInputElement>)=>{
-    if(e.target.value.length<CHARACTER_LENGTHS && e.target.value.length>-1)
+    if(e.target.value.length>CHARACTER_LENGTHS) return
     setCommunityName(e.target.value) 
     setCharacterRemain(CHARACTER_LENGTHS-e.target.value.length)  
 }
@@ -29,7 +29,8 @@ const CreateCommunityModal:React.FC<CreateCommunityModalProps> = ({open, handleO
           <Text fontSize={11} color="gray.500">Community name including capitalization cannot be changed</Text>
           <Text position={"relative"} top={"28px"} left="10px" width={"20px"} color="gray.400">r/</Text>
           <Input position={"relative"} size={"sm"} pl={"22px"} onChange={handleOnChange} value={communityName}/>
-          <Text>{characterRemains} Characters remaining </Text>
+          <Text fontSize={"9pt"} color={characterRemains===0?"red":
+        "gray.500"}>{characterRemains} Characters remaining </Text>
         </ModalBody>
         </Box>
         <ModalFooter>
